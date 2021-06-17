b9011307e4a8cbde66294e2a0c5002d76f4c3b07
Dmitry <dobikdmitry@gmail.com>
2021-06-17 19:17:42 +0300

    Homework. Lesson2

--- Lesson2/Lesson2.3.py
+++ Lesson2/Lesson2.3.py
@@ -1,9 +1,8 @@
 v = int(input('Enter speed of Vasia: '))
 t = int(input('Enter time of movement of Vasya: '))
 
-l = v * t
-if v>=0:
+l = v * t % 100
+if v >= 0:
     print(l)
 else:
-    print(100+l)
-
+    print(100 + l)
