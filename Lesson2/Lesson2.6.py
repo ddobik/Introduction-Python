e019dc9066181d0cb763c5367933c8ef2c7816e4
Dmitry <dobikdmitry@gmail.com>
2021-06-21 01:04:25 +0300

    Homework. Lesson2

--- Lesson2/Lesson2.6.py
+++ Lesson2/Lesson2.6.py
@@ -2,6 +2,6 @@ good_nums = (10, 20, 30, 40, 50, 60, 70, 80, 90, 100)
 x = float(input('Enter x: '))
 y = abs(x)
 if y in good_nums:
-    print("Yep, it is here :)")
+    print('Yep, it is here :)')
 else:
-    print("Oops, it is not here :(")
+    print('Oops, it is not here :(')
