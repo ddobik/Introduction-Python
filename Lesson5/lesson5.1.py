f094e040a2dd3498724e787ed172018498d6a6ff
Dmitry <dobikdmitry@gmail.com>
2021-07-09 23:56:22 +0300

    Homework. Lesson4

---
+++ Lesson5/lesson5.1.py
@@ -0,0 +1,12 @@
+def is_prime(x):
+    k = 0
+    for num in range(2, x):
+        if x % num == 0:
+            k += 1
+    if k == 0:
+        print('True')
+    else:
+        print('False')
+
+
+is_prime(87)
