3c87e8ac2ea804890db49ec5212233fb46d3d5de
Your Name <you@example.com>
2021-07-26 16:18:08 +0300

    Homework. Lesson5

--- Lesson6/lesson6_3.py
+++ Lesson6/lesson6_3.py
@@ -4,8 +4,10 @@ print(dict(enumerate(list(input(str('Ведите стороку:'))))))
 
 # 2) Вариант с использованием функции
 def lis_dict(x):
-    print(dict(enumerate(x)))
+    return dict(enumerate(x))
+
+    # print(dict(enumerate(x)))
 
 
 l = list(input(str('Ведите стороку:')))
-lis_dict(l)
+print(lis_dict(l))
